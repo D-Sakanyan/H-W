9d217b87a35c963db07d9c901290606ba752d2f3
D-Sakanyan <davidsakanyan2003@gmail.com>
2024-09-25 19:38:24 +0000

    111

--- Desktop/Complex_sort/Sorting.cpp
+++
@@ -1,8 +0,0 @@
-#include "Sorting.h"
-#include <algorithm>
-
-void sortComplexNumbers(std::vector<ComplexNumber>& cnArray) {
-    std::sort(cnArray.begin(), cnArray.end(), [](const ComplexNumber& a, const ComplexNumber& b) {
-        return a.absolute() < b.absolute();
-    });
-}
--- Desktop/Complex_sort/Sorting.h
+++
@@ -1,9 +0,0 @@
-#ifndef SORTING_H
-#define SORTING_H
-
-#include <vector>
-#include "Complex.h"
-
-void sortComplexNumbers(std::vector<ComplexNumber>& cnArray);
-
-#endif
