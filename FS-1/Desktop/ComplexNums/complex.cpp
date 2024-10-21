8ac187d6763ecd86d1070a48926211a2d9549da5
D-Sakanyan <davidsakanyan2003@gmail.com>
2024-10-21 14:51:16 +0000

    FS-1

---
+++ FS-1/Desktop/ComplexNums/complex.cpp
@@ -0,0 +1,26 @@
+#include "complex.h"
+#include <iostream>
+
+Complex::Complex(double r, double i) : real(r), imag(i) {}
+
+Complex Complex::operator+(const Complex& other) const {
+    return Complex(real + other.real, imag + other.imag);
+}
+
+Complex Complex::operator-(const Complex& other) const {
+    return Complex(real - other.real, imag - other.imag);
+}
+
+Complex Complex::operator*(double scalar) const {
+    return Complex(real * scalar, imag * scalar);
+}
+
+double Complex::abs() const {
+    return std::sqrt(real * real + imag * imag);
+}
+
+std::ostream& operator<<(std::ostream& os, const Complex& c) {
+    os << c.real << " + " << c.imag << "i";
+    return os;
+}
+
